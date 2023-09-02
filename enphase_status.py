ad86376aa06f4fc1c4a93bb8bf2ab7360fd4e3a8
Jeff Miccolis <jeff@miccolis.net>
2023-09-02 08:38:28 -0400

    Add script for enphase status monitoring

---
+++ enphase_status.py
@@ -0,0 +1,20 @@
+"""Get number of micro-inverters that are online"""
+import os
+from util.fetch_enphase import EnphaseClient
+
+
+def online_micro_count():
+    enphase = EnphaseClient(
+        os.getenv("ENPHASE_CLIENT_ID"),
+        os.getenv("ENPHASE_CLIENT_SECRET"),
+        os.getenv("ENPHASE_API_KEY"),
+    )
+    enphase.load_enphase_creds()
+
+    system_id = os.getenv("ENPHASE_SYSTEM_ID")
+
+    micros = enphase.v4_request(f"systems/{system_id}/devices", {})["devices"]["micros"]
+    return len(list(filter(lambda v: v["status"] == "normal", micros)))
+
+
+print(online_micro_count())
